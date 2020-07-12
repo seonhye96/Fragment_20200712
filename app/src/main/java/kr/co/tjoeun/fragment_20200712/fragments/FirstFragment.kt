67fbfdc4889f4cde04222dea676cec00b8066301
황선혜 <ghkdtjs6451@gmail.com>
2020-07-12 14:43:01 +0900

    프래그먼트 클래스 / XML 연결

--- app/src/main/java/kr/co/tjoeun/fragment_20200712/fragments/FirstFragment.kt
+++ app/src/main/java/kr/co/tjoeun/fragment_20200712/fragments/FirstFragment.kt
@@ -1,9 +1,20 @@
 package kr.co.tjoeun.fragment_20200712.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kr.co.tjoeun.fragment_20200712.R
 
 class FirstFragment : Fragment() {
 
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)  // 뒤에 null을 쓰면안된다!! 대신 container 넣기  // 여기까지 연결해둔것
+    }
 
 }
